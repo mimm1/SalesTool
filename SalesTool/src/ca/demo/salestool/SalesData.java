1e8e08334c4b8fd5562262ed577343ad4461620d
mimm1 <mmadrees@uod.edu.sa>
2015-10-18 13:10:57 +0300

    Initial Checking Sales tool

---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,14 @@
+package ca.demo.salestool;
+
+/**
+ * Represent the sales data for a given period.
+ */
+public class SalesData {
+	int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("ToDo: Implement data display ");
+
+			}
+
+}
